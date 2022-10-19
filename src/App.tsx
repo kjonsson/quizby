bc822ae05a07aa63ebb99a806e2487ca52df8fd5
Kristjan Jonsson <kris@anglingiq.com>
2022-10-19 01:15:35 +0000

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,6 @@ import { useState } from "react";
 import Game from "./Game";
 
 const App = () => {
-  const [isPlaying, setIsPlaying] = useState(true);
-
-  if (!isPlaying) {
-    return (
-      <div className="flex h-full  min-h-full w-full flex-col items-center justify-center bg-gray-800 px-4">
-        <div>TODO SET SETTINGS</div>
-        <button
-          onClick={() => {
-            setIsPlaying(true);
-          }}
-        >
-          Play game?
-        </button>
-      </div>
-    );
-  }
-
   return <Game />;
 };
 
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useMemo, useState } from "react";
 import { Question as QuestionType } from "./types";
-import { shuffle, sum, unescape } from "lodash";
+import { shuffle } from "lodash";
 import sanitizeHtml from "sanitize-html";
 
-const QUIZ_API_BASE_URL = "https://opentdb.com/api.php";
 const NUMBER_OF_QUESTIONS = 5;
-
-const BACKGROUND_COLOR = "rgb(26, 26, 26)";
-const QUESTION_BACKGROUND_COLOR = "rgb(38, 38, 38)";
-const BUTTON_COLOR = "rgb(80, 70, 230)";
 
 const getQuestions = async (numberOfQuestions: number) => {
   const res = await fetch(
@@ -18,24 +13,16 @@ const getQuestions = async (numberOfQuestions: number) => {
   return data.results;
 };
 
-const decodeHtmlEntity = (input: string) => {
-  const output = input.replace(/&#(\d+);/g, (match, dec) => {
-    return String.fromCharCode(dec);
-  });
-
-  console.log(input, "vs", output);
-
-  return output;
-};
-
 const Question = ({
   onClickAnswer,
   question,
   questionNumber,
+  currentAnswer,
 }: {
   onClickAnswer: (answer: string, questionNumber: number) => void;
   question: QuestionType;
   questionNumber: number;
+  currentAnswer: string;
 }) => {
   const answers = useMemo(() => {
     return shuffle([...question.incorrect_answers, question.correct_answer]);
@@ -50,14 +37,15 @@ const Question = ({
       ></div>
       <div className="flex flex-col">
         {answers.map((answer) => (
-          <div className="flex items-center rounded border border-gray-700 pl-4">
+          <div
+            className="my-2 flex items-center rounded border border-gray-700 bg-gray-800 pl-4 hover:cursor-pointer"
+            onClick={() => onClickAnswer(answer, questionNumber)}
+          >
             <input
-              id="bordered-radio-1"
               type="radio"
-              value=""
-              name="bordered-radio"
-              className="h-4 w-4 border-gray-600 bg-gray-700 text-blue-600  ring-offset-gray-800 focus:ring-blue-600"
-              onClick={() => onClickAnswer(answer, questionNumber)}
+              checked={currentAnswer === answer}
+              className="h-4 w-4 border-gray-600 bg-gray-700 ring-offset-gray-800 "
+              readOnly
             />
             <div
               className="ml-2 w-full py-4 text-sm font-medium text-white"
@@ -70,28 +58,74 @@ const Question = ({
   );
 };
 
+enum GAME_STATES {
+  NOT_STARTED,
+  PLAYING,
+  GAME_OVER,
+}
+
+const Questions = ({
+  answers,
+  handleClickAnswer,
+  handleClickEndGame,
+  questions,
+}: {
+  answers: string[];
+  handleClickAnswer: (answer: string, questionNumber: number) => void;
+  handleClickEndGame: () => void;
+  questions: QuestionType[];
+}) => {
+  return (
+    <div className="flex min-h-full w-full   flex-col items-center justify-center px-4 py-16">
+      {questions.map((question, questionIdx) => (
+        <Question
+          key={questionIdx}
+          question={question}
+          onClickAnswer={handleClickAnswer}
+          questionNumber={questionIdx}
+          currentAnswer={answers[questionIdx]}
+        />
+      ))}
+      <button onClick={handleClickEndGame}>Submit</button>
+    </div>
+  );
+};
+
+const GameOver = ({
+  correctAnswers,
+  handleClickPlayAgain,
+  totalQuestions,
+}: {
+  correctAnswers: number;
+  handleClickPlayAgain: () => void;
+  totalQuestions: number;
+}) => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center px-4 py-16 text-4xl text-white">
+      <div>
+        Answered {correctAnswers} / {totalQuestions}
+      </div>
+      <button onClick={handleClickPlayAgain}>Play again</button>
+    </div>
+  );
+};
+
 const Game = () => {
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [answers, setAnswers] = useState<string[]>([]);
+  const [gameState, setGameState] = useState<GAME_STATES>(GAME_STATES.PLAYING);
 
   useEffect(() => {
     async function _getQuestions(NUMBER_OF_QUESTIONS: number) {
       const questionsResult = await getQuestions(NUMBER_OF_QUESTIONS);
-      console.log("questions Result", questionsResult);
       setQuestions(questionsResult);
       const newAnswers = new Array(questionsResult.length).fill("");
       setAnswers(newAnswers);
     }
-    _getQuestions(NUMBER_OF_QUESTIONS);
-  }, []);
-
-  if (!(questions.length > 0)) {
-    return (
-      <div className="min-h-full w-full bg-gray-800 px-4 text-white">
-        Loading ...
-      </div>
-    );
-  }
+    if (gameState === GAME_STATES.PLAYING) {
+      _getQuestions(NUMBER_OF_QUESTIONS);
+    }
+  }, [gameState]);
 
   const handleClickAnswer = (answer: string, questionNumber: number) => {
     const newAnswers = answers.slice();
@@ -99,17 +133,46 @@ const Game = () => {
     setAnswers(newAnswers);
   };
 
+  const handleClickEndGame = () => {
+    setGameState(GAME_STATES.GAME_OVER);
+  };
+
+  const handleClickPlayAgain = () => {
+    setQuestions([]);
+    setAnswers([]);
+    setGameState(GAME_STATES.PLAYING);
+  };
+
+  const calculateTotal = (questions: QuestionType[], answers: string[]) => {
+    let total = 0;
+
+    questions.map((question, questionIdx) => {
+      if (question.correct_answer === answers[questionIdx]) {
+        total += 1;
+      }
+    });
+
+    return total;
+  };
+
   return (
-    <div className="min-h-full w-full   bg-gray-800 px-4">
-      <div className="flex min-h-full w-full   flex-col items-center justify-center bg-gray-800 px-4 py-16">
-        {questions.map((question, questionIdx) => (
-          <Question
-            question={question}
-            onClickAnswer={handleClickAnswer}
-            questionNumber={questionIdx}
-          />
-        ))}
-      </div>
+    <div className="min-h-full w-full   bg-gray-900 px-4">
+      {(gameState === GAME_STATES.PLAYING ||
+        gameState === GAME_STATES.NOT_STARTED) && (
+        <Questions
+          answers={answers}
+          handleClickAnswer={handleClickAnswer}
+          handleClickEndGame={handleClickEndGame}
+          questions={questions}
+        />
+      )}
+      {gameState === GAME_STATES.GAME_OVER && (
+        <GameOver
+          correctAnswers={calculateTotal(questions, answers)}
+          totalQuestions={questions.length}
+          handleClickPlayAgain={handleClickPlayAgain}
+        />
+      )}
     </div>
   );
 };
